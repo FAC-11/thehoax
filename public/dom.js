0314abd537dad6808a70117271a9064ceca26566
Max Gerber <mgerber94@gmail.com>
2017-08-10 18:24:53 +0100

    Added name to cookie and to tinfoild Relates #31

--- public/dom.js
+++ public/dom.js
@@ -11,14 +11,16 @@ function httpRequest (url, reqType, cb) {
   xhr.send();
 }
 
-
 httpRequest('/tinfoild', 'GET', function(err, data) {
-  if (err) console.log(err);
-  console.log(data);
+
 });
 
+var getGreeting = function() {
+  var cookie = document.cookie;
+  var username = cookie.split('&')[1].split('=')[1];
+  var greeting = document.getElementById('user-welcome');
+  greeting.textContent = username;
+};
 
-
-
-
+getGreeting();
 // httpRequest(url, 'get', '/getData' cb)
